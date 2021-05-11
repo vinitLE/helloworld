4b5e160e0c5a5481455443fe3640b6e04d4ad3c9
vinitLE <71550354+vinitLE@users.noreply.github.com>
2021-05-11 21:17:20 +0530

    m

---
+++ p2/java/index.js
@@ -0,0 +1,18 @@
+VanillaTilt.init(document.querySelector(".card1"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare":1
+});
+VanillaTilt.init(document.querySelector(".card2"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare":1
+});
+VanillaTilt.init(document.querySelector(".card3"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare":1
+});
