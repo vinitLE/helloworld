3dee5c54edbc245985656608d72022cc6e0f9f12
vinitLE <71550354+vinitLE@users.noreply.github.com>
2021-05-21 13:16:05 +0530

    m

---
+++ p3/js/index.js
@@ -0,0 +1,13 @@
+VanillaTilt.init(document.querySelector("#img11"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare":1
+});
+VanillaTilt.init(document.querySelector("#img12"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare":1
+});
+
